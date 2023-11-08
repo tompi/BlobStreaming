8e6ad4d595356bc238027ff8bff34a0a626f08b1
Thomas Haukland <thomas.haukland@gmail.com>
2023-11-08 12:12:14 +0100

    Range requests and local file works

--- BlobStreaming/Program.cs
+++ BlobStreaming/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.MapGet("/hello", () => "Hello World!");
+app.MapGet("/stream", () =>
+{
+    var video = File.OpenRead("testfilm.mp4");
+
+    return Results.Stream(video, "video/mp4", enableRangeProcessing: true);
+});
 
 app.Run();
